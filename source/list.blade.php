ff5fa316cf0649e8815bc68c588efbeff4f15da0
PANDAD <daijianpu@gmail.com>
2022-09-26 11:33:52 +0800

    Update list.blade.php

--- source/list.blade.php
+++ source/list.blade.php
@@ -1,14 +1,12 @@
 @extends('_layouts.master')
 
-@section('title', '四百八十四')
+@section('title', '商品一覧')
 
 @section('content')
-<h1>小说</h1>
-<h3>纯属虚构，请勿当真。</h3>
-<li><a href="/posts/tzyy" >《桃之妖妖》</a></li>
-<li>《豆腐西尸》</li>
-<li>《人工智能路西法》</li>
+<h1>商品一覧</h1>
+<h3>商品一覧</h3>
  <ul>
- <li><a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" style="text-decoration:none;">向我发送留言</a></li>
+ <a href="/order">ご注文方法</a>
+ <li><a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" style="text-decoration:none;">お問い合わせ</a></li>
     </ul>
 @endsection

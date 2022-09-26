4cad4e7f34ce7b7d11b1c79d16f527fc9f974e47
PANDAD <daijianpu@gmail.com>
2022-09-26 11:29:36 +0800

    Update order.blade.php

--- source/order.blade.php
+++ source/order.blade.php
@@ -1,14 +1,11 @@
 @extends('_layouts.master')
 
-@section('title', '四百八十四')
+@section('title', 'ご注文方法')
 
 @section('content')
-<h1>小说</h1>
-<h3>纯属虚构，请勿当真。</h3>
-<li><a href="/posts/tzyy" >《桃之妖妖》</a></li>
-<li>《豆腐西尸》</li>
-<li>《人工智能路西法》</li>
- <ul>
- <li><a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" style="text-decoration:none;">向我发送留言</a></li>
+<h1>ご注文方法</h1>
+<h3>eメールでのお問い合わせ</h3>
+ スムーズな返信のためにも、メール本文にご氏名や該当する注文番号などを記載してください。<br>
+ <li><a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" style="text-decoration:none;">送信</a></li>
     </ul>
 @endsection

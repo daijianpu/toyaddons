8c7246192f57c7deefc1d901a498f09a25b2e188
PANDAD <daijianpu@gmail.com>
2022-09-26 16:30:02 +0800

    Update list.blade.php

--- source/list.blade.php
+++ source/list.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <h1>商品一覧</h1>
 <h3>商品一覧</h3>
-<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeKGnvV0wPaFkSMwL3aJw7NktODb10gSVirmOMO42wV0Gpv9Q/viewform?embedded=true" width="800" height="1600" frameborder="0" marginheight="0" marginwidth="0">正在加载…</iframe>
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeKGnvV0wPaFkSMwL3aJw7NktODb10gSVirmOMO42wV0Gpv9Q/viewform?embedded=true" width="800" height=100% frameborder="0" marginheight="0" marginwidth="0">正在加载…</iframe>
  <ul>
  <a href="/order">ご注文方法</a>
  <li><a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" style="text-decoration:none;">お問い合わせ</a></li>

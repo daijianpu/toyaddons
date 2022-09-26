b72b9912b1943c7ecf106472ddffaee791c48e2b
PANDAD <daijianpu@gmail.com>
2022-09-26 16:04:30 +0800

    Update contact.blade.php

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -4,5 +4,5 @@
 
 @section('content')
     <h1>お問い合せは</h1>
-    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc0Yy1XtTB7o5yGnduce6bTc-iK0AsRk1ZOcZZI_kB2fUddHg/viewform?embedded=true" width="600" height="500" frameborder="0" marginheight="0" marginwidth="0">正在加载…</iframe>
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc0Yy1XtTB7o5yGnduce6bTc-iK0AsRk1ZOcZZI_kB2fUddHg/viewform?embedded=true" width="800" height="900" frameborder="0" marginheight="0" marginwidth="0">正在加载…</iframe>
 @endsection

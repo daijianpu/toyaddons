db4be4a9b13838565ad07f1cbca016599d985b55
PANDAD <daijianpu@gmail.com>
2022-09-26 10:35:39 +0800

    Create list.blade.php

---
+++ source/list.blade.php
@@ -0,0 +1,14 @@
+@extends('_layouts.master')
+
+@section('title', '四百八十四')
+
+@section('content')
+<h1>小说</h1>
+<h3>纯属虚构，请勿当真。</h3>
+<li><a href="/posts/tzyy" >《桃之妖妖》</a></li>
+<li>《豆腐西尸》</li>
+<li>《人工智能路西法》</li>
+ <ul>
+ <li><a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" style="text-decoration:none;">向我发送留言</a></li>
+    </ul>
+@endsection

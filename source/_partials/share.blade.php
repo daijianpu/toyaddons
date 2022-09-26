4314746d7a537e2d48e23fcaaeb6068907846730
PANDAD <daijianpu@gmail.com>
2022-09-26 10:57:07 +0800

    Update share.blade.php

--- source/_partials/share.blade.php
+++ source/_partials/share.blade.php
@@ -1,15 +1,1 @@
-<button data-sharer="facebook" data-url="{{ $page->getUrl() }}" style="background: #3b5998;">
-    Facebook
-</button>
-
-<button data-sharer="twitter" data-url="{{ $page->getUrl() }}" data-title="{{ $page->title }}" style="background: #00aced;">
-    Twitter
-</button>
-
-<button data-sharer="reddit" data-url="{{ $page->getUrl() }}" style="background: #ff4500;">
-    Reddit
-</button>
-
-<button data-sharer="linkedin" data-url="{{ $page->getUrl() }}" style="background: #0077b5;">
-    LinkedIn
-</button>
+&bull; <small>发表于{{ $page->prettyDate('Y-m-d') }}日。</small>

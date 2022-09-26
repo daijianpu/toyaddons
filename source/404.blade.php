55b8c750f313dc819b7f1262c042f71f18efbfa9
PANDAD <daijianpu@gmail.com>
2022-09-26 10:30:59 +0800

    Update 404.blade.php

--- source/404.blade.php
+++ source/404.blade.php
@@ -8,6 +8,5 @@ permalink: 404.html
 
 @section('content')
     <h1>404 Error</h1>
-
-    <p>The page you were trying to access could not be found.</p>
+<p><a href="/contact">お問い合せは</a></p>
 @endsection

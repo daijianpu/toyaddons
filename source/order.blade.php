bdb3b94afe526848d51756ec1ed879d96ffe9166
PANDAD <daijianpu@gmail.com>
2022-09-27 14:59:37 +0800

    Update order.blade.php

--- source/order.blade.php
+++ source/order.blade.php
@@ -4,13 +4,14 @@
 
 @section('content')
 <h1>ご注文方法</h1>
-<h2><a target="_blank" href="https://toyaddons.com/list" alt="商品一覧">商品一覧</a></h2>
+<h3><a target="_blank" href="https://toyaddons.com/list" alt="商品一覧">1商品選択→2レコード番号→3メール送信3→4返信待ち
+</a></h3>
 <hr style="border-color: silver"/>
 <img src="https://img.alicdn.com/imgextra/i2/2710706468/O1CN01EEztTi1xeOXJs0U6J_!!2710706468.gif" width="800" alt="ご注文方法" />
 <hr style="border-color: silver"/>
-<img src="https://img.alicdn.com/imgextra/i2/2710706468/O1CN01LIQMuH1xeOXLi9b6X_!!2710706468.gif" width="800" alt="ご注文方法" />
+<img src="https://img.alicdn.com/imgextra/i2/2710706468/O1CN01nQkHul1xeOXFZnsVv_!!2710706468.gif" width="800" alt="ご注文方法" />
 <hr style="border-color: silver"/>
-<img src="https://img.alicdn.com/imgextra/i4/2710706468/O1CN01iIHjnv1xeOXMU4lgr_!!2710706468.gif" width="800" alt="ご注文方法" />
+<img src="https://img.alicdn.com/imgextra/i3/2710706468/O1CN01tfMJLN1xeOXHqA3Gq_!!2710706468.gif" width="800" alt="ご注文方法" />
 <hr style="border-color: silver"/>
 <img src="https://img.alicdn.com/imgextra/i4/2710706468/O1CN01vz95CG1xeOXHfD3PJ_!!2710706468.gif" width="800" alt="ご注文方法" />
 <hr style="border-color: silver"/>

a4ef552d88578acebcb40b1f32e4af0a86ba1929
PANDAD <daijianpu@gmail.com>
2022-09-27 11:06:26 +0800

    Update order.blade.php

--- source/order.blade.php
+++ source/order.blade.php
@@ -5,7 +5,11 @@
 @section('content')
 <h1>ご注文方法</h1>
 <h3>eメールでのお問い合わせ</h3>
- スムーズな返信のためにも、メール本文にご氏名や該当する注文番号などを記載してください。<br>
+<img src="https://img.alicdn.com/imgextra/i2/2710706468/O1CN01QtRPzs1xeOXAxbukH_!!2710706468.gif" width="800" alt="ご注文方法" />
+<img src="https://img.alicdn.com/imgextra/i2/2710706468/O1CN01LIQMuH1xeOXLi9b6X_!!2710706468.gif" width="800" alt="ご注文方法" />
+<img src="https://img.alicdn.com/imgextra/i4/2710706468/O1CN01iIHjnv1xeOXMU4lgr_!!2710706468.gif" width="800" alt="ご注文方法" />
+<img src="https://img.alicdn.com/imgextra/i4/2710706468/O1CN01vz95CG1xeOXHfD3PJ_!!2710706468.gif" width="800" alt="ご注文方法" />
+<img src="https://img.alicdn.com/imgextra/i4/2710706468/O1CN01vz95CG1xeOXHfD3PJ_!!2710706468.gif" width="800" alt="ご注文方法" />
  <li><a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" style="text-decoration:none;">送信</a></li>
     </ul>
 @endsection

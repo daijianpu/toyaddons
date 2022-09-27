6c2b013689f0fe7ada35c2b03ffd5910aeacb349
PANDAD <daijianpu@gmail.com>
2022-09-27 11:46:27 +0800

    Update order.blade.php

--- source/order.blade.php
+++ source/order.blade.php
@@ -4,12 +4,13 @@
 
 @section('content')
 <h1>ご注文方法</h1>
-<h3>eメールでのお問い合わせ</h3>
+<h3><a target="_blank" href="https://toyaddons.com/list" alt="商品一覧">商品一覧</a></h3>
 <img src="https://img.alicdn.com/imgextra/i2/2710706468/O1CN01EEztTi1xeOXJs0U6J_!!2710706468.gif" width="800" alt="ご注文方法" />
 <img src="https://img.alicdn.com/imgextra/i2/2710706468/O1CN01LIQMuH1xeOXLi9b6X_!!2710706468.gif" width="800" alt="ご注文方法" />
 <img src="https://img.alicdn.com/imgextra/i4/2710706468/O1CN01iIHjnv1xeOXMU4lgr_!!2710706468.gif" width="800" alt="ご注文方法" />
 <img src="https://img.alicdn.com/imgextra/i4/2710706468/O1CN01vz95CG1xeOXHfD3PJ_!!2710706468.gif" width="800" alt="ご注文方法" />
 <img src="https://img.alicdn.com/imgextra/i4/2710706468/O1CN01vz95CG1xeOXHfD3PJ_!!2710706468.gif" width="800" alt="ご注文方法" />
+<li><a target="_blank" href="https://toyaddons.com/list" alt="商品一覧">商品一覧</a></li>
  <li><a target="_blank" href="https://toyaddons.com/contact" alt="お問い合わせ">お問い合わせ</a></li>
  </ul>
 @endsection

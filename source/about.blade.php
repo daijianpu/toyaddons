e3345e17e1d5d3bf375d0c0e4a4ead1ee396d956
PANDAD <daijianpu@gmail.com>
2022-09-27 11:53:19 +0800

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -4,7 +4,9 @@
 
 @section('content')
     <h1>会社概要</h1>
+<hr style="border-color: silver"/>
 <p>ToyAddons</p>
+<hr style="border-color: silver"/>
 <h2>Links:</h2>
 <ul>
         <li><a href="/www.Twitter.com" target="_blank">Twitter</a></li>

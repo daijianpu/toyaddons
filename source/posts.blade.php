7774d78646fe0a49a6bcd23edeb7f7d4a15343bf
PANDAD <daijianpu@gmail.com>
2022-09-27 11:58:17 +0800

    Update posts.blade.php

--- source/posts.blade.php
+++ source/posts.blade.php
@@ -4,13 +4,14 @@
 
 @section('content')
     <h1>新品情報</h1>
-
+<hr style="border-color: silver"/>
     <ul>
         @forelse ($posts->sortBy('title', 'desc') as $post)
             <li>
                 <a href="{{ $post->getPath() }}">{{ $post->title }}</a>
                 <small>{{ $post->prettyDate() }}</small>
             </li>
+            <hr style="border-color: silver"/>
         @empty
             <p><a href="/contact">お問い合わせ</a></p>
         @endforelse

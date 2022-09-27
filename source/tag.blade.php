0a55e71d86e18c5e055ab7fbe21deb2d76756a62
PANDAD <daijianpu@gmail.com>
2022-09-27 15:57:46 +0800

    Update tag.blade.php

--- source/tag.blade.php
+++ source/tag.blade.php
@@ -4,6 +4,7 @@
 
 @section('content')
     <h1>Tags</h1>
+    <hr style="border-color: silver"/>
  <ul>
   <li><a href="/tags/%E6%84%9F%E6%82%9F" >感悟</a></li>
   <li><a href="/tags/%E6%95%99%E8%82%B2" >教育</a></li>
@@ -11,6 +12,6 @@
   <li><a href="/tags/%E6%8A%80%E6%9C%AF" >技术</a></li>
   <li><a href="/tags/%E5%9B%BD%E9%99%85" >国际</a></li>
   <li><a href="/tags/%E7%BB%8F%E5%95%86" >经商</a></li> 
-  <li><a href="/posts/" >全部</a></li>
+  <li><a href="/posts/" >商品一覧</a></li>
     </ul>
 @endsection

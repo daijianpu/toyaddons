1976629530b1016716450681299ff56738ea27e4
PANDAD <daijianpu@gmail.com>
2022-09-29 15:17:45 +0800

    Update order.blade.php

--- source/order.blade.php
+++ source/order.blade.php
@@ -3,9 +3,10 @@
 @section('title', 'ご注文方法')
 
 @section('content')
-<h1>ご注文方法</h1>
-<h3><a target="_blank" href="https://toyaddons.com/list" alt="商品一覧">1商品選択→2レコード番号→3メール送信→4返信待ち
-</a></h3>
+<h1>ご利用ガイド</h1>
+<h2>1.ご注文の流れ</h2>
+<h3><li><a target="_blank" href="https://toyaddons.com/list" alt="商品一覧">1.商品選択→2.レコード番号→3.メール送信→4.返信待ち
+</a></li></h3>
 <hr style="border-color: silver"/>
 <img src="https://img.alicdn.com/imgextra/i2/2710706468/O1CN01EEztTi1xeOXJs0U6J_!!2710706468.gif" width="800" alt="ご注文方法" />
 <hr style="border-color: silver"/>
@@ -15,7 +16,8 @@
 <hr style="border-color: silver"/>
 <img src="https://img.alicdn.com/imgextra/i4/2710706468/O1CN01vz95CG1xeOXHfD3PJ_!!2710706468.gif" width="800" alt="ご注文方法" />
 <hr style="border-color: silver"/>
-<h2><li><a target="_blank" href="https://toyaddons.com/list" alt="商品一覧">商品一覧</a></li></h2>
- <h2><li><a target="_blank" href="https://toyaddons.com/contact" alt="お問い合わせ">お問い合わせ</a></li></h2>
+<h2><li><a target="_blank" href="https://toyaddons.com/list" alt="お支払い方法">2.お支払い方法</a></li></h2>
+ <h2><li><a target="_blank" href="https://toyaddons.com/contact" alt="送料">3.送料</a></li></h2>
+  <h2><li><a target="_blank" href="https://toyaddons.com/contact" alt="配達時間">4.配達時間</a></li></h2>
  </ul>
 @endsection

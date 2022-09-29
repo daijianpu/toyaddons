353d02593c03ca2b21aff9ddf81301532332b792
PANDAD <daijianpu@gmail.com>
2022-09-29 17:05:20 +0800

    Update contact.blade.php

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -6,5 +6,6 @@
     <h1>お問い合せは</h1>
     <hr style="border-color: silver"/>
     <img src="https://img.alicdn.com/imgextra/i4/2710706468/O1CN01USaoks1xeOXOrhsEi_!!2710706468.gif" width="780" alt="お問合せ" />
+       <br>
 問題にぶつかったら、ためらわないでください。私たちに連絡して、助けを得てください！
 @endsection

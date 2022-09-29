4ddc6d434d2bccdf844c03098635169a38cc0fa0
PANDAD <daijianpu@gmail.com>
2022-09-29 14:11:25 +0800

    Update tag.blade.php

--- source/tag.blade.php
+++ source/tag.blade.php
@@ -6,12 +6,12 @@
     <h1>分類一覧</h1>
     <hr style="border-color: silver"/>
  <ul>
+ <li><a href="/tags/kamenrider" >仮面ライダー</a></li>
+ <li><a href="/tags/ultraman" >ウルトラマン</a></li>
   <li><a href="/tags/geats" >仮面ライダーギーツ</a></li>
-  <li><a href="/tags/decker" >ウルトラマンデッカー</a></li>
-  <li><a href="/tags/kamenrider" >仮面ライダー</a></li>
-  <li><a href="/tags/ultraman" >ウルトラマン</a></li>
   <li><a href="/tags/revice" > 仮面ライダーリバイス</a></li>
-  <li><a href="/tags/trigger" >ウルトラマントリガー</a></li>
-  <li><a href="/posts/" >商品一覧</a></li>
+  <li><a href="/tags/decker" >ウルトラマンデッカー</a></li>
+ <li><a href="/tags/trigger" >ウルトラマントリガー</a></li>
+  <li><a href="/list" >商品一覧</a></li>
     </ul>
 @endsection

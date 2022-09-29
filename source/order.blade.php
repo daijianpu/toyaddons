5139499b785dddeaa16b1f123b8ad0f5ec70f311
PANDAD <daijianpu@gmail.com>
2022-09-29 16:49:27 +0800

    Update order.blade.php

--- source/order.blade.php
+++ source/order.blade.php
@@ -8,16 +8,24 @@
 <h3><li><a target="_blank" href="https://toyaddons.com/list" alt="商品一覧">1.商品選択→2.レコード番号→3.メール送信→4.返信待ち
 </a></li></h3>
 <hr style="border-color: silver"/>
-<img src="https://img.alicdn.com/imgextra/i2/2710706468/O1CN01EEztTi1xeOXJs0U6J_!!2710706468.gif" width="800" alt="ご注文方法" />
+<img src="https://img.alicdn.com/imgextra/i2/2710706468/O1CN01EEztTi1xeOXJs0U6J_!!2710706468.gif" width="800" alt="商品選択" />
 <hr style="border-color: silver"/>
-<img src="https://img.alicdn.com/imgextra/i2/2710706468/O1CN01nQkHul1xeOXFZnsVv_!!2710706468.gif" width="800" alt="ご注文方法" />
+<img src="https://img.alicdn.com/imgextra/i2/2710706468/O1CN01nQkHul1xeOXFZnsVv_!!2710706468.gif" width="800" alt="レコード番号" />
 <hr style="border-color: silver"/>
-<img src="https://img.alicdn.com/imgextra/i3/2710706468/O1CN01tfMJLN1xeOXHqA3Gq_!!2710706468.gif" width="800" alt="ご注文方法" />
+<img src="https://img.alicdn.com/imgextra/i3/2710706468/O1CN01tfMJLN1xeOXHqA3Gq_!!2710706468.gif" width="800" alt="メール送信" />
 <hr style="border-color: silver"/>
-<img src="https://img.alicdn.com/imgextra/i4/2710706468/O1CN01vz95CG1xeOXHfD3PJ_!!2710706468.gif" width="800" alt="ご注文方法" />
+<img src="https://img.alicdn.com/imgextra/i4/2710706468/O1CN01vz95CG1xeOXHfD3PJ_!!2710706468.gif" width="800" alt="ご返信待ち" />
 <hr style="border-color: silver"/>
-<h2><li><a target="_blank" href="https://toyaddons.com/list" alt="お支払い方法">2.お支払い方法</a></li></h2>
- <h2><li><a target="_blank" href="https://toyaddons.com/contact" alt="送料">3.送料</a></li></h2>
-  <h2><li><a target="_blank" href="https://toyaddons.com/contact" alt="配達時間">4.配達時間</a></li></h2>
+<h2><li><a target="_blank" href="/contact" alt="お支払い方法">2.お支払い方法</a></li></h2>
+<img src="https://img.alicdn.com/imgextra/i3/2710706468/O1CN01EMta6M1xeOXPlRCv0_!!2710706468.gif" width="800" alt="お支払い方法" />
+
+ <h2><li><a target="_blank" href="/contact" alt="送料">3.送料</a></li></h2>
+ <img src="https://img.alicdn.com/imgextra/i1/2710706468/O1CN0178L5cN1xeOXFDLm0J_!!2710706468.gif" width="800" alt="送料" />
+ 
+  <h2><li><a target="_blank" href="/contact" alt="配達時間">4.配達時間</a></li></h2>
+  <img src="https://img.alicdn.com/imgextra/i4/2710706468/O1CN01znJefX1xeOXNkIkcP_!!2710706468.gif" width="800" alt="ご配達時間" />
+
+  <h2><li><a target="_blank" href="/contact" alt="お問い合せは">4.お問い合せは</a></li></h2>
+  <img src="https://img.alicdn.com/imgextra/i4/2710706468/O1CN01YVRLff1xeOXFDKld6_!!2710706468.gif" width="800" alt="お問い合せは" />
  </ul>
 @endsection

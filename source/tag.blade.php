a185d16495320f3a23bbaaaccdd39866f80f2ace
PANDAD <daijianpu@gmail.com>
2022-09-30 09:06:02 +0800

    Update tag.blade.php

--- source/tag.blade.php
+++ source/tag.blade.php
@@ -6,13 +6,13 @@
     <h1>分類一覧</h1>
  <hr style="border-color: silver"/>
 <ul>
-<li><a href="/tags/addons" >Addons</a></li>
-<li><a href="/tags/shf" >SHF</a></li>
-<li><a href="/tags/csm" >CSM</a></li>
-<li><a href="/tags/ur" >UR</a></li>
+<h2><li><a href="/tags/addons" >Addons</a></li></h2>
+<h2><li><a href="/tags/shf" >SHF</a></li></h2>
+<h2><li><a href="/tags/csm" >CSM</a></li></h2>
+<h2><li><a href="/tags/ur" >UR</a></li></h2>
  <br>
 <hr style="border-color: silver"/>
-<h2><li><a href="/tags/kamenrider" >仮面ライダーKamenRider</a></li><h2>
+<h2><li><a href="/tags/kamenrider" >仮面ライダーKamenRider</a></li></h2>
   <li><a href="/tags/geats" >仮面ライダーギーツGeats</a></li>
   <li><a href="/tags/revice" > 仮面ライダーリバイスRevice</a></li>
   <li><a href="/tags/saber" > 仮面ライダーセイバーsaber</a></li>
@@ -22,7 +22,7 @@
  <li><a href="/tags/exaid" >仮面ライダーエグゼイドExaid</a></li>
 <br>
 <hr style="border-color: silver"/> 
-<h2><li><a href="/tags/ultraman" >ウルトラマンUltraman</a></li><h2>
+<h2><li><a href="/tags/ultraman" >ウルトラマンUltraman</a></li></h2>
 <li><a href="/tags/decker" >ウルトラマンデッカーDecker</a></li>
  <li><a href="/tags/trigger" >ウルトラマントリガーTrigger</a></li>
 <li><a href="/list" >商品一覧</a></li>

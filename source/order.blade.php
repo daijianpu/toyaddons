32f2df97764f0e9cc43f9351ec293beb85bf9b4f
PANDAD <daijianpu@gmail.com>
2022-09-30 16:29:18 +0800

    Update order.blade.php

--- source/order.blade.php
+++ source/order.blade.php
@@ -11,11 +11,11 @@
 <hr style="border-color: silver"/>
 <a href="/list"><img src="https://toyaddons.files.wordpress.com/2022/09/toyaddons_01.jpg" width="800" alt="商品選択" /></a>
 <hr style="border-color: silver"/>
-<a href="/list"><img src="https://toyaddons.files.wordpress.com/2022/09/toyaddons_042.jpg" width="800" alt="レコード番号" /></a>
+<a href="/list"><img src="https://toyaddons.files.wordpress.com/2022/09/22.jpg" width="800" alt="レコード番号" /></a>
 <hr style="border-color: silver"/>
-<a href="/list"><img src="https://toyaddons.files.wordpress.com/2022/09/toyaddons_05.jpg" width="800" alt="メール送信" /></a>
+<a href="/list"><img src="https://toyaddons.files.wordpress.com/2022/09/toyaddons_042.jpg" width="800" alt="メール送信" /></a>
 <hr style="border-color: silver"/>
-<a href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw"><img src="https://img.alicdn.com/imgextra/i4/2710706468/O1CN01vz95CG1xeOXHfD3PJ_!!2710706468.gif" width="800" alt="ご返信待ち" /></a>
+<a href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw"><img src="https://toyaddons.files.wordpress.com/2022/09/toyaddons_05.jpg" width="800" alt="ご返信待ち" /></a>
 <hr style="border-color: silver"/>
 <h2><li><a target="_blank" href="/contact" alt="お支払い方法">2.お支払い方法</a></li></h2>
 <img src="https://img.alicdn.com/imgextra/i3/2710706468/O1CN01EMta6M1xeOXPlRCv0_!!2710706468.gif" width="800" alt="お支払い方法" />

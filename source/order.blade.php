e350c5f0224f8e65a5402db5d6a6f3809b8291c8
PANDAD <daijianpu@gmail.com>
2022-09-30 10:21:42 +0800

    Update order.blade.php

--- source/order.blade.php
+++ source/order.blade.php
@@ -13,9 +13,9 @@
 <hr style="border-color: silver"/>
 <a href="/list"><img src="https://img.alicdn.com/imgextra/i2/2710706468/O1CN01nQkHul1xeOXFZnsVv_!!2710706468.gif" width="800" alt="レコード番号" /></a>
 <hr style="border-color: silver"/>
-<a href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw"><img src="https://img.alicdn.com/imgextra/i3/2710706468/O1CN01tfMJLN1xeOXHqA3Gq_!!2710706468.gif" width="800" alt="メール送信" /></a>
+<a href="/list"><img src="https://img.alicdn.com/imgextra/i3/2710706468/O1CN01tfMJLN1xeOXHqA3Gq_!!2710706468.gif" width="800" alt="メール送信" /></a>
 <hr style="border-color: silver"/>
-<img src="https://img.alicdn.com/imgextra/i4/2710706468/O1CN01vz95CG1xeOXHfD3PJ_!!2710706468.gif" width="800" alt="ご返信待ち" />
+<a href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw"><img src="https://img.alicdn.com/imgextra/i4/2710706468/O1CN01vz95CG1xeOXHfD3PJ_!!2710706468.gif" width="800" alt="ご返信待ち" /></a>
 <hr style="border-color: silver"/>
 <h2><li><a target="_blank" href="/contact" alt="お支払い方法">2.お支払い方法</a></li></h2>
 <img src="https://img.alicdn.com/imgextra/i3/2710706468/O1CN01EMta6M1xeOXPlRCv0_!!2710706468.gif" width="800" alt="お支払い方法" />

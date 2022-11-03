c6816eb850771ac2226c5cc48ccf8a779f58460b
PANDAD <daijianpu@gmail.com>
2022-11-03 15:05:35 +0800

    Update contact.blade.php

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -1,9 +1,9 @@
 @extends('_layouts.master')
 
-@section('title', 'お問い合せは')
+@section('title', 'お問い合せはContact')
 
 @section('content')
-    <h1>お問い合せは</h1>
+    <h1>お問い合せはContact</h1>
     <hr style="border-color: silver"/>
        <br>
 <a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" style="text-decoration:none;">→お問い合せはContact←</a>

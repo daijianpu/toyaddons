574e130b59e5d2802965c77b3d536ae14f765e8c
PANDAD <daijianpu@gmail.com>
2022-11-03 14:59:06 +0800

    Update contact.blade.php

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -5,9 +5,6 @@
 @section('content')
     <h1>お問い合せは</h1>
     <hr style="border-color: silver"/>
-    <a href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw"><img src="https://img.alicdn.com/imgextra/i4/2710706468/O1CN01USaoks1xeOXOrhsEi_!!2710706468.gif" width="780" alt="お問合せ" /></a>
        <br>
-問題にぶつかったら、ためらわないでください。<a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" style="text-decoration:none;">私たちに連絡して</a>、助けを得てください！
-<br>
-<a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" style="text-decoration:none;">→お問い合せは←</a>
+<a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" style="text-decoration:none;">→お問い合せはContact←</a>
 @endsection

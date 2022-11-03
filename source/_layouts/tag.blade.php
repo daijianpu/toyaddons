2d42f91a3fee771c84f4504d3eaa4a0b35349e92
PANDAD <daijianpu@gmail.com>
2022-11-03 15:31:11 +0800

    Update tag.blade.php

--- source/_layouts/tag.blade.php
+++ source/_layouts/tag.blade.php
@@ -1,9 +1,9 @@
 @extends('_layouts.master')
 
-@section('title', "分類'{$page->name()}'")
+@section('title', "分類Tag'{$page->name()}'")
 
 @section('content')
-    <h1>分類'{{ $page->name() }}'</h1>
+    <h1>分類Tag'{{ $page->name() }}'</h1>
 <hr style="border-color: silver"/>
     <ul>
         @forelse ($posts->filter->hasTag($page->name()) as $post)
@@ -12,7 +12,7 @@
                 <small>{{ $post->prettyDate('Y-m-d') }}</small>
             </li>
         @empty
-            <p><a href="/contact">お問い合せは</a></p>
+            <p><a href="/contact">お問い合せはContact</a></p>
         @endforelse
     </ul>
 @endsection

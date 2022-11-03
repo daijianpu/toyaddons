8746e5a0b00bf81c3dee0dd7baa791767f0601f8
PANDAD <daijianpu@gmail.com>
2022-11-03 16:20:25 +0800

    Update contact.blade.php

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -4,7 +4,6 @@
 
 @section('content')
     <h1>お問い合せはContact</h1>
-    <hr style="border-color: silver"/>
-       <br>
-<a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" style="text-decoration:none;">→お問い合せはContact←</a>
+    <hr style="border-color: silver"/> 
+<a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" style="text-decoration:none;">→お問い合せはContact←</a><br>
 @endsection

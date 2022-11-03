8367a5fac41b06b880a464de4039cd0c53004f6d
PANDAD <daijianpu@gmail.com>
2022-11-03 15:07:26 +0800

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,7 +1,7 @@
 @extends('_layouts.master')
 
 @section('content')
-<a href="https://toyaddons.com/posts">
+<a href="/contact">
   <img src="https://www.bandai.co.jp/candy/published/bnc_files/product/p4v/000000340899IvQ51uJ29fkWaL2xVWRwnFeVqCtj0qCLCp4v.jpg" width="810"/>
 </a>
 @endsection

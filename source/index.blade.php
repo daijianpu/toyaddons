695fc2bef1f96d1c78b6f4e8818b41c55e37c579
PANDAD <daijianpu@gmail.com>
2022-11-03 16:17:32 +0800

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
 <a href="/contact">
-  <img src="https://www.bandai.co.jp/candy/published/bnc_files/product/p4v/000000340899IvQ51uJ29fkWaL2xVWRwnFeVqCtj0qCLCp4v.jpg" width="810"/>
+  <img src="https://toyaddons.files.wordpress.com/2022/11/4hack444.jpg" width="810"/>
 </a>
 @endsection

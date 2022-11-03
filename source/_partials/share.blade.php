af18f9d6c7a3eda93179d5d65deda0fd5a811b37
PANDAD <daijianpu@gmail.com>
2022-11-03 15:25:19 +0800

    Update share.blade.php

--- source/_partials/share.blade.php
+++ source/_partials/share.blade.php
@@ -1,1 +1,1 @@
-&bull; <small>時間:{{ $page->prettyDate('Y-m-d') }}日&bull;<a href="/order">ご注文方法</a>&bull;<a href="/contact">お問い合わせ</a></small>
+&bull; <small>時間time:{{ $page->prettyDate('Y-m-d') }}日&bull;<a href="/order">注文方法OrderStep</a>&bull;<a href="/contact">お問い合わせContact</a></small>

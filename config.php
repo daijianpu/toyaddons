c6011f6ebcecb18453ffc3a49a98491820d134ad
PANDAD <daijianpu@gmail.com>
2022-11-03 14:27:21 +0800

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return [
     'production' => false,
     'baseUrl' => 'https://artisanstatic.netlify.app',
     'site' => [
-        'title' => 'Toyaddons',
+        'title' => 'Tehaccho',
         'description' => 'BANDAI|万代|玩具|ganggu|特撮|仮面ライダー|ウルトラマン|怪獣|食玩|ガチャ|戦隊',
         'image' => 'https://cdn-apac.onetrust.com/logos/2591b06d-75db-4383-b236-fa5d91f83cf0/f21b36e5-f55c-4a4e-a1a9-6079d2f176b3/abdbc6ee-cc1c-4fbb-b622-55092caf83f8/%E3%82%A2%E3%82%BB%E3%83%83%E3%83%88_1-100.jpg',
     ],

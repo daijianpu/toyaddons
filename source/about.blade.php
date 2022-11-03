af05113639e4f1a41ac22da2c030adb34ed19771
PANDAD <daijianpu@gmail.com>
2022-11-03 15:00:47 +0800

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -1,17 +1,14 @@
 @extends('_layouts.master')
 
-@section('title', 'ToyAddons会社概要')
+@section('title', '会社概要AboutUs')
 
 @section('content')
-    <h1>会社概要</h1>
+    <h1>会社概要AboutUs</h1>
 <hr style="border-color: silver"/>
 <p>ToyAddons</p>
 <hr style="border-color: silver"/>
 <h2>Links:</h2>
 <ul>
-        <li><a href="/www.Twitter.com" target="_blank">Twitter</a></li>
-        <li><a href="www.Instagram.com" target="_blank">Instagram</a></li>
-        <li><a href="www.Instagram.com" target="_blank">Telegram</a></li>
-        <li><a href="www.Instagram.com" target="_blank">お問合せ</a></li>
+<li><a href="/contact" target="_blank">お問合せ</a></li>
     </ul>
 @endsection

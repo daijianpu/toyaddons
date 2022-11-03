e6437cd8fcc37b5bd53708c2cbdad294fe13febf
PANDAD <daijianpu@gmail.com>
2022-11-03 15:04:56 +0800

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -5,10 +5,15 @@
 @section('content')
     <h1>会社概要AboutUs</h1>
 <hr style="border-color: silver"/>
-<p>ToyAddons</p>
+<p>私たちは2014年に設立された中国の国際貿易会社です。</br>
+We are a Chinese international trading company, established in 2014.</br>
+誠実さと効率性は、私たちの仕事のスタイルです。</br>
+Honesty and efficiency is our style.</br>
+安心して私たちの製品を購入してください！</br>
+Please feel free to buy our products!</br>
 <hr style="border-color: silver"/>
 <h2>Links:</h2>
 <ul>
-<li><a href="/contact" target="_blank">お問合せ</a></li>
+<li><a href="/contact" target="_blank">お問合せContact</a></li>
     </ul>
 @endsection
